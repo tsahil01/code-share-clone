fd483409862382ddc45a1da25da257a4a88ecb70
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-02-22 22:14:52 +0530

    add tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import './App.css'
 import { io }  from "socket.io-client";
 const socket = io("http://localhost:3000/");
 
--- frontend/src/Monaco.jsx
+++ frontend/src/Monaco.jsx
@@ -1,37 +1,41 @@
 import Editor from '@monaco-editor/react';
 import { useEffect, useState } from 'react'
 
-import { io }  from "socket.io-client";
+import { io } from "socket.io-client";
 const socket = io("http://localhost:3000/");
 
-const Monaco = function() {
-    const [value, setValue] = useState("Hello");
-    useEffect(()=>{
-        socket.on("user-connected", (code)=>{
-        console.log(code);
-        setValue(code);
+const Monaco = function () {
+    const [value, setValue] = useState();
+    useEffect(() => {
+        socket.on("user-connected", (code) => {
+            console.log(code);
+            setValue(code);
         })
-        socket.on("coding", (e)=>{
+        socket.on("coding", (e) => {
             setValue(e);
         })
     }, [])
 
-    return <>
-    <h1>Code Editor</h1>
-    <hr />
-    <Editor 
-        height="80vh" 
-        defaultLanguage="javascript" 
-        theme='vs-dark'
-        value={value}
-        onChange={(e)=> {
-            console.log(e);
-            const newValue = e;
-            setValue(newValue);
-            socket.emit("code", newValue);
-        }}
-        />;
-        </>
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-gray-800 via-gray-900 to-black text-white">
+            <h1 className='text-2xl md:text-4xl p-4 font-bold mb-8'>🚀 Live Coding! ✨</h1>
+            <div className="w-4/5 bg-opacity-70 bg-gradient-to-r from-gray-700 via-gray-800 to-gray-900 rounded-lg shadow-lg overflow-hidden">
+                <Editor
+                    height="80vh"
+                    defaultLanguage="javascript"
+                    theme='vs-dark'
+                    value={value}
+                    onChange={(e) => {
+                        console.log(e);
+                        const newValue = e;
+                        setValue(newValue);
+                        socket.emit("code", newValue);
+                    }}
+                    defaultValue='// Write your code here'
+                />
+            </div>
+        </div>
+    );
 }
 
-export default Monaco
+export default Monaco;
