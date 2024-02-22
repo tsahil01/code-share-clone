163b773281277f8da22d2c843b825f275fe8cc61
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-02-22 20:10:11 +0530

    Socket with ReactJS

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const PORT = 3000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:5173", // Update this with the origin of your React app
+      methods: ["GET", "POST"]
+    }
+  });
+
+  let code = "";
+
+io.on("connection", (socket) => {
+    console.log("User connected: ", socket.id); // x8WIv7-mJelg7on_ALbx
+    socket.emit("user-connected", code)
+
+    socket.on('disconnect', () => {
+        console.log("User disconnected")
+    })
+    socket.on('code', (val)=>{
+      socket.broadcast.emit("coding", val);
+      code = val;
+    })
+  });
+
+app.get('/', (req, res)=>{
+    res.json({
+        msg: "Helllo"
+    })
+})
+
+server.listen(PORT, () => console.log(`Listening on ${PORT}`));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { io }  from "socket.io-client";
+const socket = io("http://localhost:3000/");
+
+
+function App() {
+  const [value, setValue] = useState("");
+
+  useEffect(()=>{
+    socket.on("user-connected", (code)=>{
+      console.log(code);
+      setValue(code);
+    })
+  }, [])
+  socket.on("coding", (e)=>{
+    setValue(e);
+  })
+
+  return (
+    <>
+    <textarea cols={100} rows={50} value={value}
+     onChange={(e)=> {
+      const newValue = e.target.value;
+      setValue(newValue);
+      socket.emit("code", newValue);
+    }}/>
+    </>
+  )
+}
+
+export default App
